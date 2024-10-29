a90bda25744abdc32e3cfda9eee72c9737d8c76e
Jordan C <jordan.c4922@gmail.com>
2024-10-29 15:01:12 -0600

    Initial Commit

---
+++ Emulator Translation/Emulator_TranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emulator_TranslationApp.swift
+//  Emulator Translation
+//
+//  Created by Jordan Christensen on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Emulator_TranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
